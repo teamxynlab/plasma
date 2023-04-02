09271febc1ab34f0f879fffcc24602299fc593e4
xyntechx <xyntechx@gmail.com>
2023-04-02 08:48:41 +0800

    Init app

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+    return (
+        <p class="text-4xl text-indigo-500 text-center py-20">Hello Plasma!</p>
+    );
+};
+
+export default App;
