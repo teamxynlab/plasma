e2eff9042be57e3e1842e340b4d55e0c821bba34
xyntechx <xyntechx@gmail.com>
2023-04-03 09:32:41 +0800

    Implement router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-import type { Component } from "solid-js";
+import { Component } from "solid-js";
+import { lazy } from "solid-js";
+import { Routes, Route, A } from "@solidjs/router";
+
+const Home = lazy(() => import("./routes/home"));
 
 const App: Component = () => {
     return (
-        <p class="text-4xl text-indigo-500 text-center py-20">Hello Plasma!</p>
+        <div class=" w-screen min-h-screen flex items-center justify-center flex-col">
+            <Routes>
+                <Route path="/" component={Home} />
+
+                {/* More routes here */}
+
+                <Route
+                    path="/*"
+                    element={
+                        <p>
+                            Oops! This page doesn't exist.{" "}
+                            <A
+                                href="/"
+                                class="p-0 m-0 underline transition-colors hover:text-indigo-500"
+                            >
+                                Back to home
+                            </A>
+                            .
+                        </p>
+                    }
+                />
+            </Routes>
+        </div>
     );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 /* @refresh reload */
-import "./index.css";
 import { render } from "solid-js/web";
-
+import { Router } from "@solidjs/router";
 import App from "./App";
+import "./index.css";
 
 const root = document.getElementById("root");
 
@@ -12,4 +12,11 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
     );
 }
 
-render(() => <App />, root!);
+render(
+    () => (
+        <Router>
+            <App />
+        </Router>
+    ),
+    root!
+);
