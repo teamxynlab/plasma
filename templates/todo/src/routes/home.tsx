6aa12bcd7d87ce98f7e8e974cc8a69176ec71795
xyntechx <xyntechx@gmail.com>
2023-04-10 14:58:45 +0800

    Complete TODO app

--- templates/todo/src/routes/home.tsx
+++ templates/todo/src/routes/home.tsx
@@ -1,9 +1,10 @@
-import { Component, For, createSignal } from "solid-js";
+import { Component, For, createSignal, onMount } from "solid-js";
 import { createStore } from "solid-js/store";
 import { pb } from "../../utils/pocketbase";
 
 interface TodoProps {
-    id: number;
+    id?: string;
+    index: number;
     content: string;
     isDone: boolean;
 }
@@ -11,35 +12,90 @@ interface TodoProps {
 const Home: Component = () => {
     const [newTodo, setNewTodo] = createSignal("");
 
-    const [todos, setTodos] = createStore<TodoProps[]>([
-        {
-            id: 0,
-            content: "Try Plasma out",
-            isDone: true,
-        },
-        {
-            id: 1,
-            content: "Add more TODOs",
-            isDone: false,
-        },
-        {
-            id: 2,
-            content: "Experiment with the Plasma TODO app",
-            isDone: false,
-        },
-    ]);
+    const [todos, setTodos] = createStore<TodoProps[]>([]);
 
-    const createTodo = () => {
-        setTodos([
-            ...todos,
+    onMount(async () => {
+        let records = await pb.collection("todos").getFullList({
+            sort: "created",
+        });
+
+        const data = [
             {
-                id: todos.length,
-                content: newTodo(),
+                index: 0,
+                content: "Try Plasma out",
+                isDone: true,
+            },
+            {
+                index: 1,
+                content: "Add more TODOs",
                 isDone: false,
             },
-        ]);
+            {
+                index: 2,
+                content: "Experiment with the Plasma TODO app",
+                isDone: false,
+            },
+        ];
+
+        if (records.length === 0) {
+            for (let i = 0; i < data.length; i++) {
+                await pb.collection("todos").create(data[i]);
+            }
+        }
+
+        records = await pb.collection("todos").getFullList({
+            sort: "created",
+        });
+
+        for (let i = 0; i < records.length; i++) {
+            setTodos([
+                ...todos,
+                {
+                    id: records[i].id,
+                    index: records[i].index,
+                    content: records[i].content,
+                    isDone: records[i].isDone,
+                },
+            ]);
+        }
+    });
+
+    const createTodo = async () => {
+        const data = {
+            index: todos.length,
+            content: newTodo(),
+            isDone: false,
+        };
+
+        setTodos([...todos, data]);
+
+        await pb.collection("todos").create(data);
 
         setNewTodo("");
+    };
+
+    const updateTodoContent = async (e, id, index) => {
+        setTodos(
+            (todo) => todo.index === index,
+            "content",
+            e.currentTarget.value
+        );
+
+        await pb.collection("todos").update(id, {
+            content: e.currentTarget.value,
+        });
+    };
+
+    const updateTodoIsDone = async (id, index) => {
+        setTodos(
+            (todo) => todo.index === index,
+            "isDone",
+            (isDone) => !isDone
+        );
+
+        await pb.collection("todos").update(id, {
+            isDone: todos[index].isDone,
+        });
     };
 
     return (
@@ -51,23 +107,11 @@ const Home: Component = () => {
                         <input
                             placeholder={`TODO #${i() + 1}`}
                             value={todo.content}
-                            onChange={(e) =>
-                                setTodos(
-                                    (todo) => todo.id === i(),
-                                    "content",
-                                    () => e.currentTarget.value
-                                )
-                            }
+                            onChange={(e) => updateTodoContent(e, todo.id, i())}
                             class="flex-1 outline-none bg-gray-100 p-4 rounded-s-lg"
                         />
                         <button
-                            onClick={() =>
-                                setTodos(
-                                    (todo) => todo.id === i(),
-                                    "isDone",
-                                    (isDone) => !isDone
-                                )
-                            }
+                            onClick={() => updateTodoIsDone(todo.id, i())}
                             class={
                                 todo.isDone
                                     ? "transition-colors bg-green-200 hover:bg-green-300 p-4 rounded-e-lg w-[120px]"
